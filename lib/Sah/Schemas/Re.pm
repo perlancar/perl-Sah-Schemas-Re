b2493421eca0e0b4534935abe0d6a48507d0a277
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-09-03 18:21:01 +0700

    [doc] Mention some related modules/schemas

--- lib/Sah/Schemas/Re.pm
+++ lib/Sah/Schemas/Re.pm
@@ -13,3 +13,11 @@ use strict;
 =head1 append:SEE ALSO
 
 L<Sah::PSchemas::Re>
+
+L<Sah::Schemas::RegexpPattern>
+
+=head2 Related Sah schemas from L<Sah::Schemas::Str> distribution
+
+L<Sah::Schema::str_or_re>
+
+L<Sah::Schema::str_or_re_or_code>

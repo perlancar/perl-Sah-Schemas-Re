c29fee37c9a861498d64b68d5ea876519f163a29
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-12-20 09:41:46 +0700

    [doc] Explain the difference between re_from_str and str_or_re

--- lib/Sah/Schema/re_from_str.pm
+++ lib/Sah/Schema/re_from_str.pm
@@ -10,7 +10,7 @@ use strict;
 our $schema = [
     re => {
         summary => 'Regexp object from string using Regexp::From::String\'s str_to_re()',
-        description => <<'_',
+        description => <<'MARKDOWN',
 
 This schema accepts Regexp object or string which will be coerced to Regexp object
 using <pm:Regexp::From::String>'s `str_to_re()` function.
@@ -20,7 +20,13 @@ specify metacharacters as if you are writing a literal regexp pattern in Perl.
 Otherwise, your string will be `quotemeta()`-ed first then compiled to Regexp
 object. This means in the second case you cannot specify metacharacters.
 
-_
+What's the difference between this schema and `str_or_re` (from
+<pm:Sah::Schemas::Str>)? Both this schema and `str_or_re` accept string, but
+this schema will still coerce strings not in the form of `/.../` or `qr(...)` to
+regexp object, while `str_or_re` will leave the string as-is. In other words,
+this schema always converts input to Regexp object while `str_or_re` does not.
+
+MARKDOWN
 
         prefilters => [ ['Re::re_from_str'=>{}] ],
 
